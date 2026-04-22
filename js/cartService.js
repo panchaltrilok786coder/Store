d1c13493ac54234d048d7732045afd8a4a75581c
panchaltrilok786coder <panchaltrilok786@gmail.com>
2026-04-22 19:54:34 +0530

    Update cartService.js

--- js/cartService.js
+++ js/cartService.js
@@ -8,18 +8,43 @@ import {
   updateDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
 // ADD
 export async function addToCart(product) {
-  const user = auth.currentUser;
-  if (!user) return alert("Login required");
+  return new Promise((resolve, reject) => {
 
-  const ref = doc(db, "users", user.uid, "cart", product.id);
+    onAuthStateChanged(auth, async (user) => {
 
-  await setDoc(ref, {
-    ...product,
-    quantity: 1
+      if (!user) {
+        alert("Login required");
+        reject("No user");
+        return;
+      }
+
+      try {
+        const ref = doc(db, "users", user.uid, "cart", product.id);
+
+        await setDoc(ref, {
+          ...product,
+          quantity: 1
+        });
+
+        resolve();
+
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+
+    });
+
   });
 }
+
 
 // GET
 export async function getCart() {
@@ -34,15 +59,20 @@ export async function getCart() {
   }));
 }
 
+
 // REMOVE
 export async function removeFromCart(id) {
   const user = auth.currentUser;
+  if (!user) return;
+
   await deleteDoc(doc(db, "users", user.uid, "cart", id));
 }
+
 
 // UPDATE QUANTITY
 export async function updateQuantity(id, quantity) {
   const user = auth.currentUser;
+  if (!user) return;
 
   if (quantity <= 0) {
     await removeFromCart(id);
